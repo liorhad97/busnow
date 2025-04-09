0717edce0a50e601164ff49928c66609bdf8c46d
liorhad97 <140194760+liorhad97@users.noreply.github.com>
2025-04-09 14:22:56 +0300

    Extract animation types to separate file

---
+++ lib/presentation/widgets/animations/animation_types.dart
@@ -0,0 +1,17 @@
+/// Different types of animations for loading indicators and other animated elements
+enum AnimationType {
+  /// Pulsing animation that scales up and down
+  pulse,
+  
+  /// Rotation animation that spins continuously
+  rotate,
+  
+  /// Bouncing animation that moves up and down
+  bounce,
+  
+  /// Sliding animation that moves horizontally
+  slide,
+  
+  /// Fading animation that changes opacity
+  fade
+}
